4726339a388c1d827cebe6b1a0f7cc2dc91d63cc
Andre Campos <andreleo.campos@gmail.com>
2017-05-05 12:38:55 -0300

    Initial Commit

---
+++ Hotmart/Hotmart/Views/Main/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hotmart
+//
+//  Created by Andre Campos on 05/05/17.
+//  Copyright © 2017 Andre Campos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
