b837f3d51b4f49d877be2e6400fd82723e592324
Andre Campos <andreleo.campos@gmail.com>
2017-05-09 15:11:02 -0300

    Estrutura

---
+++ Hotmart/Hotmart/Views/Main/MainVC.swift
@@ -0,0 +1,22 @@
+//
+//  MainVC.swift
+//  Hotmart
+//
+//  Created by Andre Campos on 05/05/17.
+//  Copyright © 2017 Andre Campos. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+	
+}
